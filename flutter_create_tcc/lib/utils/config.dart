6fa8801eeb95cc7c57b2710d0f616b645ed057f1
Seungjin01 <seungjin1@hotmail.com>
2025-11-25 21:28:27 -0300

    Mudando a logo da splashscreen nativa

--- flutter_create_tcc/lib/utils/config.dart
+++ flutter_create_tcc/lib/utils/config.dart
@@ -2,7 +2,7 @@
 class Config {
   static String get baseUrl {
     // Para desenvolvimento, force o IP da máquina
-    // Mude para "http://10.0.2.2:5000/api" quando for testar no emulador
-    return "http://192.168.15.3:5000/api";
+    // Mude para "http://10.0.2.2:5000/api" quando for testar no emulador // Android "http://192.168.15.3:5000/api"
+    return "http://10.0.2.2:5000/api";
   }
 }
