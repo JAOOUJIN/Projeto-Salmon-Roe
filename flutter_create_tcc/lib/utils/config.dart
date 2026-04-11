848f7b969a14118b089e2c7f405f145643badc5f
Seungjin01 <seungjin1@hotmail.com>
2026-04-11 02:32:15 -0300

    Ajustes para rodar o render

--- flutter_create_tcc/lib/utils/config.dart
+++ flutter_create_tcc/lib/utils/config.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/foundation.dart';
+
 class Config {
   static String get baseUrl {
-    // Para desenvolvimento, force o IP da máquina
-    // Mude para "http://10.0.2.2:5000/api" quando for testar no emulador // Android "http://192.168.15.3:5000/api"
-    // Mudar para o Render quando for para produção "https://projeto-salmon-roe.onrender.com/api"
-    return "http://192.168.15.3:8080/v1/api/salmon_roe";
+    if (kReleaseMode) {
+      // URL de produção no Render
+      return "https://projeto-salmon-roe.onrender.com/v1/api/salmon_roe";
+    } else {
+      // Debug: o IP local da máquina
+      return "http://192.168.15.3:8080/v1/api/salmon_roe";
+    }
   }
 }
+// Para desenvolvimento, force o IP da máquina
+// Mude para "http://10.0.2.2:5000/api" quando for testar no emulador // Android "http://192.168.15.3:5000/api"
+// Mudar para o Render quando for para produção "https://projeto-salmon-roe.onrender.com/api"
